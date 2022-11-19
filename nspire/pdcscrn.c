4776db770c16df6ef0ebdaf79531cc8bb57c87fc
grubsteak <joel.herzog@att.net>
2022-11-19 17:25:56 -0600

    sketchy SDL_GetTicks() hack

--- nspire/pdcscrn.c
+++ nspire/pdcscrn.c
@@ -1,10 +1,10 @@
 /* Public Domain Curses */
 
-#include "pdcsdl.h"
-
 #include <stdlib.h>
+
 #include "deffont.h"
 #include "deficon.h"
+#include "pdcsdl.h"
 
 SDL_Surface *pdc_screen = NULL, *pdc_font = NULL, *pdc_icon = NULL,
             *pdc_back = NULL, *pdc_tileback = NULL;
@@ -17,27 +17,23 @@ PDC_bool pdc_own_screen;
 
 /* COLOR_PAIR to attribute encoding table. */
 
-static struct {short f, b;} atrtab[PDC_COLOR_PAIRS];
+static struct { short f, b; } atrtab[PDC_COLOR_PAIRS];
 
-void PDC_retile(void)
-{
+void PDC_retile(void) {
     if (pdc_tileback)
         SDL_FreeSurface(pdc_tileback);
 
     pdc_tileback = SDL_DisplayFormat(pdc_screen);
 
-    if (pdc_back)
-    {
+    if (pdc_back) {
         SDL_Rect dest;
 
         dest.y = 0;
 
-        while (dest.y < pdc_tileback->h)
-        {
+        while (dest.y < pdc_tileback->h) {
             dest.x = 0;
 
-            while (dest.x < pdc_tileback->w)
-            {
+            while (dest.x < pdc_tileback->w) {
                 SDL_BlitSurface(pdc_back, 0, pdc_tileback, &dest);
                 dest.x += pdc_back->w;
             }
@@ -49,21 +45,18 @@ void PDC_retile(void)
     }
 }
 
-void PDC_scr_close(void)
-{
+void PDC_scr_close(void) {
     PDC_LOG(("PDC_scr_close() - called\n"));
 }
 
-void PDC_scr_free(void)
-{
+void PDC_scr_free(void) {
     if (SP)
         free(SP);
 }
 
 /* open the physical screen -- allocate SP, miscellaneous intialization */
 
-int PDC_scr_open(int argc, char **argv)
-{
+int PDC_scr_open(int argc, char **argv) {
     int i;
 
     PDC_LOG(("PDC_scr_open() - called\n"));
@@ -75,9 +68,8 @@ int PDC_scr_open(int argc, char **argv)
 
     pdc_own_screen = !pdc_screen;
 
-    if (pdc_own_screen)
-    {
-        if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) < 0)
+    if (pdc_own_screen) {
+        if (SDL_Init(SDL_INIT_VIDEO) < 0)  // |SDL_INIT_TIMER
         {
             fprintf(stderr, "Could not start SDL: %s\n", SDL_GetError());
             return ERR;
@@ -86,8 +78,7 @@ int PDC_scr_open(int argc, char **argv)
         atexit(SDL_Quit);
     }
 
-    if (!pdc_font)
-    {
+    if (!pdc_font) {
         const char *fname = getenv("PDC_FONT");
         pdc_font = SDL_LoadBMP(fname ? fname : "pdcfont.bmp");
     }
@@ -95,27 +86,23 @@ int PDC_scr_open(int argc, char **argv)
     if (!pdc_font)
         pdc_font = SDL_LoadBMP_RW(SDL_RWFromMem(deffont, sizeof(deffont)), 0);
 
-    if (!pdc_font)
-    {
+    if (!pdc_font) {
         fprintf(stderr, "Could not load font\n");
         return ERR;
     }
 
     SP->mono = !pdc_font->format->palette;
 
-    if (!SP->mono && !pdc_back)
-    {
+    if (!SP->mono && !pdc_back) {
         const char *bname = getenv("PDC_BACKGROUND");
         pdc_back = SDL_LoadBMP(bname ? bname : "pdcback.bmp");
     }
 
-    if (!SP->mono && (pdc_back || !pdc_own_screen))
-    {
+    if (!SP->mono && (pdc_back || !pdc_own_screen)) {
         SP->orig_attr = PDC_TRUE;
         SP->orig_fore = COLOR_WHITE;
         SP->orig_back = -1;
-    }
-    else
+    } else
         SP->orig_attr = PDC_FALSE;
 
     pdc_fheight = pdc_font->h / 8;
@@ -124,21 +111,20 @@ int PDC_scr_open(int argc, char **argv)
     if (!SP->mono)
         pdc_flastc = pdc_font->format->palette->ncolors - 1;
 
-    if (pdc_own_screen && !pdc_icon)
-    {
+    if (pdc_own_screen && !pdc_icon) {
         const char *iname = getenv("PDC_ICON");
         pdc_icon = SDL_LoadBMP(iname ? iname : "pdcicon.bmp");
 
         if (!pdc_icon)
             pdc_icon = SDL_LoadBMP_RW(SDL_RWFromMem(deficon,
-                                                    sizeof(deficon)), 0);
+                                                    sizeof(deficon)),
+                                      0);
 
         if (pdc_icon)
             SDL_WM_SetIcon(pdc_icon, NULL);
     }
 
-    if (pdc_own_screen)
-    {
+    if (pdc_own_screen) {
         const char *env = getenv("PDC_LINES");
         pdc_sheight = (env ? atoi(env) : 25) * pdc_fheight;
 
@@ -146,10 +132,8 @@ int PDC_scr_open(int argc, char **argv)
         pdc_swidth = (env ? atoi(env) : 80) * pdc_fwidth;
 
         pdc_screen = SDL_SetVideoMode(pdc_swidth, pdc_sheight, 0,
-            SDL_SWSURFACE|SDL_ANYFORMAT|SDL_RESIZABLE);
-    }
-    else
-    {
+                                      SDL_SWSURFACE | SDL_ANYFORMAT | SDL_RESIZABLE);
+    } else {
         if (!pdc_sheight)
             pdc_sheight = pdc_screen->h - pdc_yoffset;
 
@@ -157,8 +141,7 @@ int PDC_scr_open(int argc, char **argv)
             pdc_swidth = pdc_screen->w - pdc_xoffset;
     }
 
-    if (!pdc_screen)
-    {
+    if (!pdc_screen) {
         fprintf(stderr, "Couldn't create a surface: %s\n", SDL_GetError());
         return ERR;
     }
@@ -166,8 +149,7 @@ int PDC_scr_open(int argc, char **argv)
     if (SP->orig_attr)
         PDC_retile();
 
-    for (i = 0; i < 8; i++)
-    {
+    for (i = 0; i < 8; i++) {
         pdc_color[i].r = (i & COLOR_RED) ? 0xc0 : 0;
         pdc_color[i].g = (i & COLOR_GREEN) ? 0xc0 : 0;
         pdc_color[i].b = (i & COLOR_BLUE) ? 0xc0 : 0;
@@ -201,13 +183,11 @@ int PDC_scr_open(int argc, char **argv)
 
 /* the core of resize_term() */
 
-int PDC_resize_screen(int nlines, int ncols)
-{
+int PDC_resize_screen(int nlines, int ncols) {
     if (!pdc_own_screen)
         return ERR;
 
-    if (nlines && ncols)
-    {
+    if (nlines && ncols) {
         pdc_sheight = nlines * pdc_fheight;
         pdc_swidth = ncols * pdc_fwidth;
     }
@@ -215,7 +195,7 @@ int PDC_resize_screen(int nlines, int ncols)
     SDL_FreeSurface(pdc_screen);
 
     pdc_screen = SDL_SetVideoMode(pdc_swidth, pdc_sheight, 0,
-        SDL_SWSURFACE|SDL_ANYFORMAT|SDL_RESIZABLE);
+                                  SDL_SWSURFACE | SDL_ANYFORMAT | SDL_RESIZABLE);
 
     if (pdc_tileback)
         PDC_retile();
@@ -226,51 +206,43 @@ int PDC_resize_screen(int nlines, int ncols)
     return OK;
 }
 
-void PDC_reset_prog_mode(void)
-{
+void PDC_reset_prog_mode(void) {
     PDC_LOG(("PDC_reset_prog_mode() - called.\n"));
 
     PDC_flushinp();
     SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
 }
 
-void PDC_reset_shell_mode(void)
-{
+void PDC_reset_shell_mode(void) {
     PDC_LOG(("PDC_reset_shell_mode() - called.\n"));
 
     SDL_EnableKeyRepeat(0, 0);
     PDC_flushinp();
 }
 
-void PDC_restore_screen_mode(int i)
-{
+void PDC_restore_screen_mode(int i) {
 }
 
-void PDC_save_screen_mode(int i)
-{
+void PDC_save_screen_mode(int i) {
 }
 
-void PDC_init_pair(short pair, short fg, short bg)
-{
+void PDC_init_pair(short pair, short fg, short bg) {
     atrtab[pair].f = fg;
     atrtab[pair].b = bg;
 }
 
-int PDC_pair_content(short pair, short *fg, short *bg)
-{
+int PDC_pair_content(short pair, short *fg, short *bg) {
     *fg = atrtab[pair].f;
     *bg = atrtab[pair].b;
 
     return OK;
 }
 
-PDC_bool PDC_can_change_color(void)
-{
+PDC_bool PDC_can_change_color(void) {
     return PDC_TRUE;
 }
 
-int PDC_color_content(short color, short *red, short *green, short *blue)
-{
+int PDC_color_content(short color, short *red, short *green, short *blue) {
     *red = DIVROUND(pdc_color[color].r * 1000, 255);
     *green = DIVROUND(pdc_color[color].g * 1000, 255);
     *blue = DIVROUND(pdc_color[color].b * 1000, 255);
@@ -278,8 +250,7 @@ int PDC_color_content(short color, short *red, short *green, short *blue)
     return OK;
 }
 
-int PDC_init_color(short color, short red, short green, short blue)
-{
+int PDC_init_color(short color, short red, short green, short blue) {
     pdc_color[color].r = DIVROUND(red * 255, 1000);
     pdc_color[color].g = DIVROUND(green * 255, 1000);
     pdc_color[color].b = DIVROUND(blue * 255, 1000);
--- nspire/pdcsdl.h
+++ nspire/pdcsdl.h
@@ -1,24 +1,26 @@
 /* Public Domain Curses */
 
+#include <SDL/SDL.h>
 #include <curspriv.h>
 
-#include <SDL/SDL.h>
+// 0x900D0000 is 100hz timer. Hopefully this won't screw stuff up.
+#define SDL_GetTicks() (Uint32)(*((unsigned short *)0x900D0000) * 10)
 
 PDCEX SDL_Surface *pdc_screen, *pdc_font, *pdc_icon, *pdc_back;
 PDCEX int pdc_sheight, pdc_swidth, pdc_yoffset, pdc_xoffset;
 
-extern SDL_Surface *pdc_tileback;    /* used to regenerate the background
+extern SDL_Surface *pdc_tileback;   /* used to regenerate the background
                                         of "transparent" cells */
-extern SDL_Color pdc_color[16];      /* colors for font palette */
-extern Uint32 pdc_mapped[16];        /* colors for FillRect(), as
+extern SDL_Color pdc_color[16];     /* colors for font palette */
+extern Uint32 pdc_mapped[16];       /* colors for FillRect(), as
                                         used in _highlight() */
-extern int pdc_fheight, pdc_fwidth;  /* font height and width */
-extern int pdc_flastc;               /* font palette's last color
+extern int pdc_fheight, pdc_fwidth; /* font height and width */
+extern int pdc_flastc;              /* font palette's last color
                                         (treated as the foreground) */
-extern PDC_bool pdc_own_screen;          /* if pdc_screen was not set
+extern PDC_bool pdc_own_screen;     /* if pdc_screen was not set
                                         before initscr(), PDCurses is
                                         responsible for (owns) it */
-extern Uint32 pdc_lastupdate;        /* time of last update, in ticks */
+extern Uint32 pdc_lastupdate;       /* time of last update, in ticks */
 
 void PDC_update_rects(void);
 void PDC_retile(void);
